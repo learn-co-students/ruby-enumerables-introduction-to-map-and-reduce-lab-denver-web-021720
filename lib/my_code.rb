76ce5251927de51e4a308219cf325981c340914e
Jonathan Higger <jonathan.higger@gmail.com>
2020-02-01 03:23:39 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -59,6 +59,12 @@ end
 
 def reduce_to_any_true(source_array)
   i=0
-  
+  while i < source_array.length
+    if source_array[i]==true
+      return true
 
+    end
+      i=i+1
+  end
+    return false
 end
